e8cb6484b38ba5f67e81524715ed197bb76cf4ae
IAGEngineers <iag-software-founders@googlegroups.com>
2026-05-07 19:31:29 +0300

    chore: cleanup bloat and binaries

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    cgiCall: (request, args) => ipcRenderer.invoke('cgi-call', { request, args }),
-    minimize: () => ipcRenderer.send('window-minimize'),
-    maximize: () => ipcRenderer.send('window-maximize'),
-    close: () => ipcRenderer.send('window-close')
-});
